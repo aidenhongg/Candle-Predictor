430680f8b4d90491f55863decf20ffef4f53d73e
Aiden Hong <aidenhongg>
2025-11-17 20:14:00 -0800

    finetune revision for multiple gpus

--- main_finetune.py
+++ main_finetune.py
@@ -1,30 +1,54 @@
 import numpy as np
 from itertools import product
-from multiprocessing import Pool
-import main_pipeline as mp
-import hyperparams as hp
-import torch
+import multiprocessing as mp
+import random
 
-NUM_PROCESSES = 5
 
-def run_with_hyperparams(params_tuple):
+NUM_PROCESSES = 10
+
+def run_with_hyperparams(args):
+    import os
+    gpu_id, params_tuple = args
     lr, decay, warmup = params_tuple
+    
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
+    import main_pipeline as pipeline
+    import hyperparams as hp
+    import torch
+    torch.cuda.set_device(0)  # After setting CUDA_VISIBLE_DEVICES, use device 0
     
     hp.LEARNING_RATE = lr
     hp.WEIGHT_DECAY = decay
     hp.WARMUP = warmup
         
-    return mp.main('training_data.csv', 'regressor', DEBUG_MODE=False, GRAPH_MODE=False)
+    return pipeline.main('training_data.csv', 'regressor', DEBUG_MODE=False, GRAPH_MODE=False)
 
-def run_seeds_reg(seed):
+def run_seeds_reg(args):
+    import os
+
+    gpu_id, seed = args
+    
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
+    import main_pipeline as pipeline
+    import hyperparams as hp
+    import torch
+    torch.cuda.set_device(0)
+    
     hp.SEED = seed
+    return pipeline.main('training_data.csv', 'regressor', DEBUG_MODE=False, GRAPH_MODE=False)
 
-    return mp.main('training_data.csv', 'regressor', DEBUG_MODE=False, GRAPH_MODE=False)
-
-def run_seeds_class(seed):
+def run_seeds_class(args):
+    import os
+    gpu_id, seed = args
+    
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
+    import main_pipeline as pipeline
+    import hyperparams as hp
+    import torch
+    torch.cuda.set_device(0)
+    
     hp.SEED = seed
-
-    return mp.main('training_data.csv', 'classifier', DEBUG_MODE=False, GRAPH_MODE=False)
+    return pipeline.main('training_data.csv', 'classifier', DEBUG_MODE=False, GRAPH_MODE=False)
 
 
 def finetune_params():
@@ -33,22 +57,28 @@ def finetune_params():
     warmups = np.arange(7000, 13000, 1000)
 
     all_hyperparams = list(product(lrs, decays, warmups))
+    
+    # Assign GPU IDs cyclically
+    tasks = [(i % NUM_PROCESSES, params) for i, params in enumerate(all_hyperparams)]
         
-    with Pool(processes=min(NUM_PROCESSES, len(all_hyperparams))) as pool:
-        pool.map(run_with_hyperparams, all_hyperparams)
+    with mp.Pool(processes=min(NUM_PROCESSES, len(all_hyperparams))) as pool:
+        pool.map(run_with_hyperparams, tasks)
 
 def seed_search():
-    seeds = [torch.randint(0, 2**32 - 1, (1,)).item() for _ in range(10)]
+    seeds = [random.randint(0, 2**32 - 1) for _ in range(10)]
+
+    tasks = [(i % NUM_PROCESSES, seed) for i, seed in enumerate(seeds)]
     
     with mp.Pool(processes=min(NUM_PROCESSES, len(seeds))) as pool:
-        pool.map(run_seeds_reg, seeds)
+        pool.map(run_seeds_reg, tasks)
 
-    seeds = [torch.randint(0, 2**32 - 1, (1,)).item() for _ in range(10)]
+    seeds = [random.randint(0, 2**32 - 1) for _ in range(10)]
+    tasks = [(i % NUM_PROCESSES, seed) for i, seed in enumerate(seeds)]
 
     with mp.Pool(processes=min(NUM_PROCESSES, len(seeds))) as pool:
-        pool.map(run_seeds_class, seeds)
+        pool.map(run_seeds_class, tasks)
 
 
 if __name__ == "__main__":
+    mp.set_start_method('spawn')
     seed_search()
-
